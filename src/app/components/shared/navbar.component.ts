4630b6dadca8d11739bea6a52e5b6d7cb0d3f318
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-03-29 18:40:43 +0800

    Added navbar

--- src/app/components/shared/navbar.component.ts
+++ src/app/components/shared/navbar.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'tbui-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatButton,
+    MatIconButton,
+  ],
   template: `
-    Navbar
+    <mat-toolbar color="primary">
+      <button mat-icon-button class="example-icon" aria-label="Example icon-button with menu icon">
+        <mat-icon>menu</mat-icon>
+      </button>
+      <span>My App</span>
+      <span class="example-spacer"></span>
+      <button mat-icon-button class="example-icon favorite-icon" aria-label="Example icon-button with heart icon">
+        <mat-icon>favorite</mat-icon>
+      </button>
+      <button mat-icon-button class="example-icon" aria-label="Example icon-button with share icon">
+        <mat-icon>share</mat-icon>
+      </button>
+    </mat-toolbar>
   `,
   styles: ``,
 })
