b170e2d3b05854c684bf8bb85e8fbd489e6642d8
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-03-29 18:24:54 +0800

    Added angular material

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { Routes, provideRouter } from '@angular/router';
 import { DashboardComponent } from './app/components/dashboard.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -10,6 +11,6 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes), provideAnimationsAsync(),
   ]
 })
